1f85269a222d35a32224ace2e5894a8fc6274cc1
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2013-09-23 09:10:11 +0200

    initial commit

---
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -0,0 +1,17 @@
+(ns ^:shared tutorial-client.behavior
+    (:require [clojure.string :as string]
+              [io.pedestal.app.messages :as msg]
+              [io.pedestal.app :as app]))
+
+(defn inc-transform [old-value _]
+  ((fnil inc 0) old-value))
+
+(def example-app
+  {:version 2
+    :transform [[:inc [:my-counter] inc-transform]]
+    :emit [{:init init-main}
+           [#{[:*]} (app/default-emitter [:main])]]})
+
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
